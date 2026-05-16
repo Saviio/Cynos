1d7c4a027fa22aff2a455508a438dc5a7581a175
Saviio <silver_Ras@163.com>
2026-05-17 01:42:09 +0800

    chore(core): sync wasm bindings

--- js/packages/core/src/wasm.d.ts
+++ js/packages/core/src/wasm.d.ts
@@ -1110,9 +1110,9 @@ export interface InitOutput {
     readonly schemalayout_nullMaskSize: (a: number) => number;
     readonly schemalayout_rowStride: (a: number) => number;
     readonly __wasm_bindgen_func_elem_88: (a: number, b: number) => void;
-    readonly __wasm_bindgen_func_elem_3664: (a: number, b: number) => void;
-    readonly __wasm_bindgen_func_elem_9178: (a: number, b: number, c: number, d: number) => void;
-    readonly __wasm_bindgen_func_elem_3671: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_3668: (a: number, b: number) => void;
+    readonly __wasm_bindgen_func_elem_9182: (a: number, b: number, c: number, d: number) => void;
+    readonly __wasm_bindgen_func_elem_3675: (a: number, b: number, c: number) => void;
     readonly __wasm_bindgen_func_elem_1555: (a: number, b: number) => void;
     readonly __wbindgen_export: (a: number, b: number) => number;
     readonly __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
